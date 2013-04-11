694b17fbd7c6c02838b45b15b93894e27c2788d7
Jeff Wintersinger <jeff.git@wintersinger.com>
2013-04-11 14:13:18 -0600

    Customize admin.

--- shipman/admin.py
+++ shipman/admin.py
@@ -1,6 +1,24 @@
 import shipman.models
 from django.contrib import admin
 
+class EmployeeAdminSpaceshipInline(admin.StackedInline):
+  model = shipman.models.Employee
+  exclude = ('hovertruck',)
+  verbose_name = 'Crew member'
+  verbose_name_plural = 'Crew members'
+
+class EmployeeAdminHovertruckInline(admin.StackedInline):
+  model = shipman.models.Employee
+  exclude = ('spaceship',)
+  verbose_name = 'Crew member'
+  verbose_name_plural = 'Crew members'
+
+class SpaceshipAdmin(admin.ModelAdmin):
+  inlines = (EmployeeAdminSpaceshipInline,)
+
+class HovertruckAdmin(admin.ModelAdmin):
+  inlines = (EmployeeAdminHovertruckInline,)
+
 admin.site.register(shipman.models.Spacesuit)
 admin.site.register(shipman.models.Employee)
 admin.site.register(shipman.models.PlanetaryBody)
@@ -8,7 +26,7 @@ admin.site.register(shipman.models.Atmosphere)
 admin.site.register(shipman.models.Location)
 admin.site.register(shipman.models.Depot)
 admin.site.register(shipman.models.Storefront)
-admin.site.register(shipman.models.Spaceship)
-admin.site.register(shipman.models.Hovertruck)
+admin.site.register(shipman.models.Spaceship, SpaceshipAdmin)
+admin.site.register(shipman.models.Hovertruck, HovertruckAdmin)
 admin.site.register(shipman.models.Customer)
 admin.site.register(shipman.models.Package)
--- shipman/models.py
+++ shipman/models.py
@@ -3,21 +3,11 @@ from django.db import models
 class Spacesuit(models.Model):
   size = models.CharField(max_length=100)
   model = models.CharField(max_length=100)
+  colour = models.CharField(max_length=100)
   class Meta:
     db_table = 'spacesuits'
-
-class Employee(models.Model):
-  first_name = models.CharField(max_length=100)
-  last_name = models.CharField(max_length=100)
-  ssn = models.CharField(max_length=10, primary_key=True)
-  supervisor = models.ForeignKey('self')
-  birthdate = models.DateTimeField()
-  salary = models.DecimalField(decimal_places=2, max_digits=15)
-  spacesuits = models.ManyToManyField(Spacesuit)
   def __unicode__(self):
-    return '%s %s' % (self.first_name, self.last_name)
-  class Meta:
-    db_table = 'employees'
+    return '%s %s %s' % (self.size, self.colour, self.model)
 
 class PlanetaryBody(models.Model):
   name = models.CharField(max_length=100)
@@ -28,6 +18,7 @@ class PlanetaryBody(models.Model):
     return self.name
   class Meta:
     db_table = 'planetary_bodies'
+    verbose_name_plural = 'planetary bodies'
 
 class Atmosphere(models.Model):
   atmosphere = models.CharField(max_length = 100)
@@ -40,7 +31,7 @@ class Location(models.Model):
   planetary_body = models.ForeignKey(PlanetaryBody)
   address = models.CharField(max_length = 400)
   def __unicode__(self):
-    return '%s on %s' % (self.address, self.planetary_body.name)
+    return '%s on %s' % (self.address, self.planetary_body.__unicode__())
   class Meta:
     db_table = 'locations'
 
@@ -61,7 +52,6 @@ class Storefront(models.Model):
     db_table = 'storefronts'
 
 class Spaceship(models.Model):
-  crew_members = models.ForeignKey(Employee)
   volume = models.FloatField()
   type = models.CharField(max_length=100)
   current_coord_x = models.FloatField()
@@ -70,9 +60,15 @@ class Spaceship(models.Model):
   destination = models.ForeignKey(Location)
   class Meta:
     db_table = 'spaceships'
+  def __unicode__(self):
+    return '%s m^3 spaceship at (%s, %s, %s)' % (
+        self.volume,
+        self.current_coord_x,
+        self.current_coord_y,
+        self.current_coord_z,
+      )
 
 class Hovertruck(models.Model):
-  crew_members = models.ForeignKey(Employee)
   volume = models.FloatField()
   current_coord_x = models.FloatField()
   current_coord_y = models.FloatField()
@@ -80,6 +76,29 @@ class Hovertruck(models.Model):
   destination = models.ForeignKey(Location)
   class Meta:
     db_table = 'hovertrucks'
+  def __unicode__(self):
+    return '%s m^3 hovertruck at (%s, %s, %s)' % (
+        self.volume,
+        self.current_coord_x,
+        self.current_coord_y,
+        self.current_coord_z,
+      )
+
+class Employee(models.Model):
+  first_name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+  ssn = models.CharField(max_length=10, primary_key=True, verbose_name='SSN')
+  supervisor = models.ForeignKey('self', blank=True, null=True)
+  birthdate = models.DateField()
+  salary = models.DecimalField(decimal_places=2, max_digits=15)
+  spacesuits = models.ManyToManyField(Spacesuit)
+  spaceship = models.ForeignKey(Spaceship, related_name='crew_members', null=True, blank=True)
+  hovertruck = models.ForeignKey(Hovertruck, related_name='crew_members', null=True, blank=True)
+  def __unicode__(self):
+    return '%s %s' % (self.first_name, self.last_name)
+  class Meta:
+    db_table = 'employees'
+
 
 class Customer(models.Model):
   first_name = models.CharField(max_length=100)
@@ -95,13 +114,13 @@ class Package(models.Model):
   height = models.FloatField()
   depth = models.FloatField()
   mass = models.FloatField()
-  spaceship = models.ForeignKey(Spaceship)
-  hovertruck = models.ForeignKey(Hovertruck)
-  storefront = models.ForeignKey(Storefront)
-  depot = models.ForeignKey(Depot)
   recipient = models.ForeignKey(Customer, related_name='received_packages')
   shipper = models.ForeignKey(Customer, related_name='shipped_packages')
+  storefront = models.ForeignKey(Storefront, null=True, blank=True)
+  depot = models.ForeignKey(Depot, null=True, blank=True)
+  spaceship = models.ForeignKey(Spaceship, null=True, blank=True)
+  hovertruck = models.ForeignKey(Hovertruck, null=True, blank=True)
   def __unicode__(self):
-    return 'Package from %s to %s' % (self.shipper, self.recipient)
+    return 'Package from %s to %s' % (self.shipper.__unicode__(), self.recipient.__unicode__())
   class Meta:
     db_table = 'packages'
